fe66a74dda734d818ed63af6b0379ec8e7228c5b
bryan vera <bveracachay@gmail.com>
2022-06-15 17:31:38 -0500

    se modifica card y se quita precio

--- js/cotizador.js
+++ js/cotizador.js
@@ -1,7 +1,7 @@
 import ubicacion from './api.js';
 
 //cambio de precios
-const precio = document.querySelector('.container-cards');
+// const precio = document.querySelector('.container-cards');
 const bandera = document.querySelector('.container-bandera');
 const precioPromo = document.querySelector('.precio-promo');
 
@@ -138,255 +138,254 @@ const cambiandoInfo = async () => {
 	const data = await ubicacion();
 	console.log('aqui toy', data);
 	const precios = country[data.country] || countryDefaults;
-	precioPromo.innerHTML = `<span class="precio-promo">${precios.money}${precios.precioPromo}</span>`;
+	// precioPromo.innerHTML = `<span class="precio-promo">${precios.money}${precios.precioPromo}</span>`;
 	bandera.innerHTML = `<img
 	src=${precios.bandera}
 	width="32px"
 	alt="bandera"
 /><p>${precios.name}</p>`;
-	console.log(precios);
-	precio.innerHTML = `<div class="card-precio">
-	<div class="card-precio__container">
-		<h3 class="card-precio__container-title">Plan Básico</h3>
-		<div class="card-precio__container-money">
-			<p>
-				${precios.basico.price} <span>${precios.money}</span>
-				<span class="card-precio__container-igv">+ IGV</span>
-			</p>
-		</div>
-		<p class="card-precio__container-money-price-after">
-			Precio normal <del>${precios.money} ${precios.basico.priceAlter}</del>
-		</p>
-	</div>
-	<div class="card-precio__container-list">
-		<div class="card-precio__container-list__item card-first-margin">
-								<img src="./img/check.svg" alt="" />
-								<p>Hosting y Dominio (Gratis)</p>
-							</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Certificado ssl</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>1 Páginas</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>1 Slider</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>3 Categorías</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>10 Imágenes (edición)</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>2 Correo corporativo</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Web responsive</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Mapa ubicación</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Integración con RRSS</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<i class="fa-solid fa-xmark fa-lg close-x"></i>
-			<p class="x-p">Web administrable</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<i class="fa-solid fa-xmark fa-lg close-x"></i>
-			<p class="x-p">Manual de usuario</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Formulario de contacto</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<i class="fa-solid fa-xmark fa-lg close-x"></i>
-			<p class="x-p">Chat online</p>
-		</div>
-		<span class="card-precio__container-list__item"
-			><i>* Renovación anual ${precios.money} ${precios.basico.renovacion}</i></span
-		>
-		<a
-			href="https://api.whatsapp.com/send?phone=51977832615&text=Hola,%20vengo%20de%20skillien.%20Me%20gustar%C3%ADa%20tener%20mas%20informaci%C3%B3n%20del%20Plan%20Basico."
-			target="_blank"
-			class="btn-comprar-card"
-			>Comprar</a
-		>
-	</div>
-</div>
-<div class="card-precio card-precio-medio">
-	<div class="card-precio__container">
-		<h3 class="card-precio__container-title">Plan Corporativo</h3>
-		<div class="card-precio__container-money">
-			<p>
-			${precios.corporativo.price}<span>${precios.money}</span>
-				<span class="card-precio__container-igv-corp">+ IGV</span>
-			</p>
-		</div>
-		<p class="card-precio__container-money-price-after">
-			Precio normal <del>${precios.money} ${precios.corporativo.priceAlter}</del>
-		</p>
-	</div>
-	<div class="card-precio__container-list">
-		<div class="card-precio__container-list__item card-first-margin">
-								<img src="./img/check.svg" alt="" />
-								<p>Hosting y Dominio (Gratis)</p>
-							</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Certificado ssl</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>5 Páginas</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>3 Slider</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>5 Categorías</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>30 Imágenes (edición)</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>10 Correo corporativo</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Web responsive</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Mapa ubicación</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Integración con RRSS</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Web administrable</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Manual de usuario</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Formulario de contacto</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="" />
-			<p>Chat online</p>
-		</div>
-		<span class="card-precio__container-list__item"
-			><i>* Renovación anual ${precios.money} ${precios.corporativo.renovacion}</i></span
-		>
-		<a
-			href="https://api.whatsapp.com/send?phone=51977832615&text=Hola,%20vengo%20de%20skillien.%20Me%20gustar%C3%ADa%20tener%20mas%20informaci%C3%B3n%20del%20Plan%20Corporativo."
-			target="_blank"
-			class="btn-comprar-card"
-			>Comprar</a
-		>
-	</div>
-</div>
-<div class="card-precio">
-	<div class="card-precio__container">
-		<h3 class="card-precio__container-title">Plan Emprendedor</h3>
-		<div class="card-precio__container-money">
-			<p>
-			${precios.emprendedor.price} <span>${precios.money}</span>
-				<span class="card-precio__container-igv">+ IGV</span>
-			</p>
-		</div>
-		<p class="card-precio__container-money-price-after">
-			Precio normal <del>${precios.money} ${precios.emprendedor.priceAlter}</del>
-		</p>
-	</div>
-	<div class="card-precio__container-list">
-	<div class="card-precio__container-list__item card-first-margin">
-	<img src="./img/check.svg" alt="" />
-	<p>Hosting y Dominio (Gratis)</p>
-</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>Certificado ssl</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>3 Páginas</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>2 Slider</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>3 Categorías</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>15 Imágenes (edición)</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>5 Correo corporativo</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>Web responsive</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>Mapa ubicación</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>Integración con RRSS</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>Web administrable</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>Manual de usuario</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<img src="./img/check.svg" alt="icon de check" />
-			<p>Formulario de contacto</p>
-		</div>
-		<div class="card-precio__container-list__item">
-			<i class="fa-solid fa-xmark fa-lg close-x"></i>
-			<p class="x-p">Chat online</p>
-		</div>
-		<span class="card-precio__container-list__item"
-			><i>* Renovación anual ${precios.money} ${precios.emprendedor.renovacion}</i></span
-		>
-		<a
-			href="https://api.whatsapp.com/send?phone=51977832615&text=Hola,%20vengo%20de%20skillien.%20Me%20gustar%C3%ADa%20tener%20mas%20informaci%C3%B3n%20del%20Plan%20Emprendedor."
-			target="_blank"
-			class="btn-comprar-card"
-			>Comprar</a
-		>
-	</div>
-</div>`;
+	// 	precio.innerHTML = `<div class="card-precio">
+	// 	<div class="card-precio__container">
+	// 		<h3 class="card-precio__container-title">Plan Básico</h3>
+	// 		<div class="card-precio__container-money">
+	// 			<p>
+	// 				${precios.basico.price} <span>${precios.money}</span>
+	// 				<span class="card-precio__container-igv">+ IGV</span>
+	// 			</p>
+	// 		</div>
+	// 		<p class="card-precio__container-money-price-after">
+	// 			Precio normal <del>${precios.money} ${precios.basico.priceAlter}</del>
+	// 		</p>
+	// 	</div>
+	// 	<div class="card-precio__container-list">
+	// 		<div class="card-precio__container-list__item card-first-margin">
+	// 								<img src="./img/check.svg" alt="" />
+	// 								<p>Hosting y Dominio (Gratis)</p>
+	// 							</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Certificado ssl</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>1 Páginas</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>1 Slider</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>3 Categorías</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>10 Imágenes (edición)</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>2 Correo corporativo</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Web responsive</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Mapa ubicación</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Integración con RRSS</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<i class="fa-solid fa-xmark fa-lg close-x"></i>
+	// 			<p class="x-p">Web administrable</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<i class="fa-solid fa-xmark fa-lg close-x"></i>
+	// 			<p class="x-p">Manual de usuario</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Formulario de contacto</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<i class="fa-solid fa-xmark fa-lg close-x"></i>
+	// 			<p class="x-p">Chat online</p>
+	// 		</div>
+	// 		<span class="card-precio__container-list__item"
+	// 			><i>* Renovación anual ${precios.money} ${precios.basico.renovacion}</i></span
+	// 		>
+	// 		<a
+	// 			href="https://api.whatsapp.com/send?phone=51977832615&text=Hola,%20vengo%20de%20skillien.%20Me%20gustar%C3%ADa%20tener%20mas%20informaci%C3%B3n%20del%20Plan%20Basico."
+	// 			target="_blank"
+	// 			class="btn-comprar-card"
+	// 			>Comprar</a
+	// 		>
+	// 	</div>
+	// </div>
+	// <div class="card-precio card-precio-medio">
+	// 	<div class="card-precio__container">
+	// 		<h3 class="card-precio__container-title">Plan Corporativo</h3>
+	// 		<div class="card-precio__container-money">
+	// 			<p>
+	// 			${precios.corporativo.price}<span>${precios.money}</span>
+	// 				<span class="card-precio__container-igv-corp">+ IGV</span>
+	// 			</p>
+	// 		</div>
+	// 		<p class="card-precio__container-money-price-after">
+	// 			Precio normal <del>${precios.money} ${precios.corporativo.priceAlter}</del>
+	// 		</p>
+	// 	</div>
+	// 	<div class="card-precio__container-list">
+	// 		<div class="card-precio__container-list__item card-first-margin">
+	// 								<img src="./img/check.svg" alt="" />
+	// 								<p>Hosting y Dominio (Gratis)</p>
+	// 							</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Certificado ssl</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>5 Páginas</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>3 Slider</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>5 Categorías</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>30 Imágenes (edición)</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>10 Correo corporativo</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Web responsive</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Mapa ubicación</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Integración con RRSS</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Web administrable</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Manual de usuario</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Formulario de contacto</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="" />
+	// 			<p>Chat online</p>
+	// 		</div>
+	// 		<span class="card-precio__container-list__item"
+	// 			><i>* Renovación anual ${precios.money} ${precios.corporativo.renovacion}</i></span
+	// 		>
+	// 		<a
+	// 			href="https://api.whatsapp.com/send?phone=51977832615&text=Hola,%20vengo%20de%20skillien.%20Me%20gustar%C3%ADa%20tener%20mas%20informaci%C3%B3n%20del%20Plan%20Corporativo."
+	// 			target="_blank"
+	// 			class="btn-comprar-card"
+	// 			>Comprar</a
+	// 		>
+	// 	</div>
+	// </div>
+	// <div class="card-precio">
+	// 	<div class="card-precio__container">
+	// 		<h3 class="card-precio__container-title">Plan Emprendedor</h3>
+	// 		<div class="card-precio__container-money">
+	// 			<p>
+	// 			${precios.emprendedor.price} <span>${precios.money}</span>
+	// 				<span class="card-precio__container-igv">+ IGV</span>
+	// 			</p>
+	// 		</div>
+	// 		<p class="card-precio__container-money-price-after">
+	// 			Precio normal <del>${precios.money} ${precios.emprendedor.priceAlter}</del>
+	// 		</p>
+	// 	</div>
+	// 	<div class="card-precio__container-list">
+	// 	<div class="card-precio__container-list__item card-first-margin">
+	// 	<img src="./img/check.svg" alt="" />
+	// 	<p>Hosting y Dominio (Gratis)</p>
+	// </div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>Certificado ssl</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>3 Páginas</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>2 Slider</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>3 Categorías</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>15 Imágenes (edición)</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>5 Correo corporativo</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>Web responsive</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>Mapa ubicación</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>Integración con RRSS</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>Web administrable</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>Manual de usuario</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<img src="./img/check.svg" alt="icon de check" />
+	// 			<p>Formulario de contacto</p>
+	// 		</div>
+	// 		<div class="card-precio__container-list__item">
+	// 			<i class="fa-solid fa-xmark fa-lg close-x"></i>
+	// 			<p class="x-p">Chat online</p>
+	// 		</div>
+	// 		<span class="card-precio__container-list__item"
+	// 			><i>* Renovación anual ${precios.money} ${precios.emprendedor.renovacion}</i></span
+	// 		>
+	// 		<a
+	// 			href="https://api.whatsapp.com/send?phone=51977832615&text=Hola,%20vengo%20de%20skillien.%20Me%20gustar%C3%ADa%20tener%20mas%20informaci%C3%B3n%20del%20Plan%20Emprendedor."
+	// 			target="_blank"
+	// 			class="btn-comprar-card"
+	// 			>Comprar</a
+	// 		>
+	// 	</div>
+	// </div>`;
 };
 cambiandoInfo();
